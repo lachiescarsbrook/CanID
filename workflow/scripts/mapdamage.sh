12beeb1e57b4940d64ed91f658169d50b51a89db
lachiescarsbrook <58501490+lachiescarsbrook@users.noreply.github.com>
2024-05-31 12:16:25 +0100

    Update mapdamage.sh

--- workflow/scripts/mapdamage.sh
+++ workflow/scripts/mapdamage.sh
@@ -3,8 +3,10 @@ BAM=$1
 REF=$2
 SAMPLE=$3
 DOWN=$4
+#Runs Mapdamage to calculate damage statistics
 mapDamage --merge-reference-sequences -i $BAM -r $REF -d results/mapdamage/${SAMPLE}
 
+#Moves all files to a shared directory
 mv results/mapdamage/${SAMPLE}/5pCtoT_freq.txt results/mapdamage/${SAMPLE}_5pCtoT_freq.txt
 mv results/mapdamage/${SAMPLE}/3pGtoA_freq.txt results/mapdamage/${SAMPLE}_3pGtoA_freq.txt
 mv results/mapdamage/${SAMPLE}/dnacomp.txt results/mapdamage/${SAMPLE}_dnacomp.txt
@@ -13,5 +15,4 @@ mv results/mapdamage/${SAMPLE}/Length_plot.pdf results/mapdamage/${SAMPLE}_Lengt
 mv results/mapdamage/${SAMPLE}/misincorporation.txt results/mapdamage/${SAMPLE}_misincorporation.txt
 mv results/mapdamage/${SAMPLE}/Runtime_log.txt results/mapdamage/${SAMPLE}_Runtime_log.txt
 mv results/mapdamage/${SAMPLE}/lgdistribution.txt results/mapdamage/${SAMPLE}_lgdistribution.txt
-
 rm -r results/mapdamage/${SAMPLE}
