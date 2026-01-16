645509916fc1df9fff88443d5494f7d6641e2559
lachiescarsbrook <58501490+lachiescarsbrook@users.noreply.github.com>
2026-01-16 12:22:50 +0000

    Update chrom_label.sh

--- workflow/scripts/chrom_label.sh
+++ workflow/scripts/chrom_label.sh
@@ -4,6 +4,7 @@ CHRX=$2
 CHRY=$3
 CHRM=$4
 
+rm workflow/files/chromosomes.txt
 touch workflow/files/chromosomes.txt
 for i in `seq 1 $CHR`; do echo "chr${i}" >> workflow/files/chromosomes.txt; done
 echo $CHRX >> workflow/files/chromosomes.txt
